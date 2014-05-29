1bc8d529289264f756cfa9ee4a2b46b0f303dbe9
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2014-05-29 12:25:01 +0100

    Aula 28 - ADO.NET intro: fechar recursos no bloco finally

---
+++ aula28_ADO_intro/NorthwindTests/NorthwindTests/TestProducts.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace NorthwindTests
+{
+    [TestClass]
+    public class TestProducts
+    {
+        [TestMethod]
+        public void test_products_count_all()
+        {
+
+            SqlConnection c = null;
+            SqlCommand cmd = null;
+            SqlDataReader dr = null;
+            try
+            {
+                c = new SqlConnection();
+                c.ConnectionString = @"
+                Data Source=DRAGAO\SQLEXPRESS;
+				Initial Catalog=Northwind;
+                Integrated Security=True";
+                c.Open();
+
+                cmd = c.CreateCommand();
+                cmd.CommandText = "SELECT [ProductID], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products]";
+
+                dr = cmd.ExecuteReader();
+                int count = 0;
+                while (dr.Read())
+                    count++;
+
+                Assert.AreEqual(77, count);
+            }
+            finally
+            {
+                dr.Dispose();
+                cmd.Dispose();
+                c.Dispose();
+            }
+        }
+    }
+}
