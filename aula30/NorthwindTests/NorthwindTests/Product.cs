5af57d9d37b2348784ec368608b24caed78be19d
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2014-06-03 13:42:44 +0100

    Aula 30 - acesso a dados relacionais com modelo de domínio.

---
+++ aula30/NorthwindTests/NorthwindTests/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTests
+{
+    public class Product 
+    {
+        public static readonly string SQL_GET_ALL = "SELECT [ProductId], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products]";
+        public static readonly string SQL_GET_BY_ID = "SELECT [ProductId], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products] WHERE ProductId = {0}";
+        public static readonly string SQL_UPDATE = "UPDATE Products SET UnitPrice = {0}, UnitsInStock = {1} WHERE ProductId = {2}";
+
+        public static string sqlGetById(int id)
+        {
+            string res = String.Format(SQL_GET_BY_ID, id);
+            return res;
+        }
+
+
+        public static string sqlUpdate(Product p)
+        {
+            return  String.Format(SQL_UPDATE, p.UnitPrice, p.UnitsInStock, p.ProductID);
+        }
+
+
+        public int ProductID { get { return prodId; } }
+        public string ProductName { set; get; }
+        public decimal UnitPrice { set; get; }
+        public short UnitsInStock { set; get; }
+
+        private readonly int prodId;
+
+        public Product(
+            int productId,
+            string productName,
+            decimal unitPrice,
+            short unitsInStock
+)
+        {
+            prodId = productId;
+            ProductName = productName;
+            UnitPrice = unitPrice;
+            UnitsInStock = unitsInStock;
+        }
+
+        public Product(SqlDataReader dr)
+        {
+            prodId = (int) dr["ProductId"];
+            ProductName = (string)dr["ProductName"];
+            UnitPrice = (decimal)dr["UnitPrice"];
+            UnitsInStock = (short)dr["UnitsInStock"];
+        }
+
+        public override string ToString()
+        {
+            return "Product [id=" + ProductID + ", name=" + ProductName + ", price=" + UnitPrice
+                    + ", stock=" + UnitsInStock + "]";
+        }
+
+    }
+
+}
---
+++ aula30/NorthwindTests/NorthwindTests/TestProducts.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace NorthwindTests
+{
+    [TestClass]
+    public class TestProducts
+    {
+
+        private SqlConnection c;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            c = new SqlConnection();
+            c.ConnectionString = @"
+                    Data Source=DRAGAO\SQLEXPRESS;
+				    Initial Catalog=Northwind;
+                    Integrated Security=True";
+            c.Open();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (c != null)
+            {
+                c.Dispose();
+                c = null;
+            }
+        }
+
+        [TestMethod]
+        public void test_products_query_count()
+        {
+            using (SqlCommand cmd = c.CreateCommand())
+            {
+                cmd.CommandText = "SELECT COUNT(*) FROM [Northwind].[dbo].[Products]";
+                int count = (int)cmd.ExecuteScalar();
+                Assert.AreEqual(77, count);
+            }
+        }
+
+        [TestMethod]
+        public void test_products_query_and_count_all()
+        {
+            using (SqlCommand cmd = c.CreateCommand())
+            {
+                cmd.CommandText = "SELECT [ProductID], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products]";
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+
+                    int count = 0;
+                    while (dr.Read())
+                        count++;
+
+                    Assert.AreEqual(77, count);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void test_product_price()
+        {
+            using (SqlTransaction trx = c.BeginTransaction())
+            {
+                using (SqlCommand cmd = c.CreateCommand())
+                {
+                    cmd.CommandText = Product.sqlGetById(7);
+                    cmd.Transaction = trx;
+                    Product p = null;
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dr.Read();
+                        p = new Product(dr);
+                        Assert.AreEqual(30.0m, p.UnitPrice);
+                    }
+
+                    p.UnitPrice = 78;
+                    cmd.CommandText = Product.sqlUpdate(p);
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = Product.sqlGetById(p.ProductID);
+                    cmd.Transaction = trx;
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dr.Read();
+                        p = new Product(dr);
+                        Assert.AreEqual(78.0m, p.UnitPrice);
+                    }
+
+                }
+                trx.Rollback();
+            }
+        }
+
+    }
+}
