9c59819e33b8c69a61dfef5ec739bf160407c4eb
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2014-06-12 11:18:28 +0100

    Aula 33 setup

---
+++ aula33_data_mapper/NorthwindTests/NorthwindDataMappers/ProductDataMapper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindDataMappers
+{
+    public class ProductDataMapper: IDataMapper<Product>
+    {
+        public static readonly string SQL_GET_ALL = "SELECT [ProductId], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products]";
+        public static readonly string SQL_GET_BY_ID = "SELECT [ProductId], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products] WHERE ProductId = @ProductId";
+        public static readonly string SQL_UPDATE = "UPDATE Products SET ProductName = @ProductName, UnitPrice = @UnitPrice, UnitsInStock = @UnitsInStock WHERE ProductId = @ProductId";
+        public static readonly string SQL_INSERT = "INSERT INTO Products (ProductName, UnitPrice, UnitsInStock) OUTPUT inserted.ProductID VALUES (@ProductName, @UnitPrice, @UnitsInStock) ";
+
+        public static SqlCommand sqlGetById(SqlConnection c)
+        {
+            SqlCommand cmd = c.CreateCommand();
+            cmd.CommandText = SQL_GET_BY_ID;
+            cmd.Parameters.Add(new SqlParameter("@ProductId", SqlDbType.Int));
+            return cmd;
+        }
+
+
+        public static SqlCommand sqlUpdate(SqlConnection c)
+        {
+            SqlCommand cmd = c.CreateCommand();
+            cmd.CommandText = SQL_UPDATE;
+            cmd.Parameters.Add(new SqlParameter("@ProductId", SqlDbType.Int));
+            cmd.Parameters.Add(new SqlParameter("@ProductName", SqlDbType.VarChar));
+            cmd.Parameters.Add(new SqlParameter("@UnitsInStock", SqlDbType.Int));
+            cmd.Parameters.Add(new SqlParameter("@UnitPrice", SqlDbType.Decimal));
+            return cmd;
+        }
+
+        public static SqlCommand sqlInsert(SqlConnection c)
+        {
+            SqlCommand cmd = c.CreateCommand();
+            cmd.CommandText = SQL_INSERT;
+            cmd.Parameters.Add(new SqlParameter("@ProductName", SqlDbType.VarChar));
+            cmd.Parameters.Add(new SqlParameter("@UnitsInStock", SqlDbType.Int));
+            cmd.Parameters.Add(new SqlParameter("@UnitPrice", SqlDbType.Decimal));
+            return cmd;
+        }
+
+        private readonly SqlConnection c;
+
+        public ProductDataMapper(SqlConnection c) {
+            this.c = c;
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            using (SqlCommand cmdGet = c.CreateCommand())
+            {
+                cmdGet.CommandText = SQL_GET_ALL;
+                using (SqlDataReader dr = cmdGet.ExecuteReader())
+                {
+                    dr.Read();
+                    Product newProd = new Product(
+                        (int) dr[0], (string) dr[1], (decimal)dr[2], (short) dr[3]);
+                    yield return newProd;
+                }
+            }
+
+        }
+
+        public void Update(Product val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Product val)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ aula33_data_mapper/NorthwindTests/NorthwindTests/TestProducts.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using NorthwindDataMappers;
+
+namespace NorthwindTests
+{
+    [TestClass]
+    public class TestProducts
+    {
+
+        private SqlConnection c;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            c = new SqlConnection();
+            c.ConnectionString = @"
+                    Data Source=DRAGAO\SQLEXPRESS;
+				    Initial Catalog=Northwind;
+                    Integrated Security=True";
+            c.Open();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (c != null)
+            {
+                c.Dispose();
+                c = null;
+            }
+        }
+
+        [TestMethod]
+        public void test_products_query_count()
+        {
+            using (SqlCommand cmd = c.CreateCommand())
+            {
+                cmd.CommandText = "SELECT COUNT(*) FROM [Northwind].[dbo].[Products]";
+                int count = (int)cmd.ExecuteScalar();
+                Assert.AreEqual(77, count);
+            }
+        }
+
+        [TestMethod]
+        public void test_products_query_and_count_all()
+        {
+            using (SqlCommand cmd = c.CreateCommand())
+            {
+                cmd.CommandText = "SELECT [ProductID], [ProductName], [UnitPrice], [UnitsInStock] FROM [Northwind].[dbo].[Products]";
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+
+                    int count = 0;
+                    while (dr.Read())
+                        count++;
+
+                    Assert.AreEqual(77, count);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void test_product_insert()
+        {
+            Product prod = new Product();
+            prod.ProductName = "Enlatados";
+            prod.UnitPrice = 78.9m;
+            prod.UnitsInStock = 100;
+
+            using (SqlTransaction trx = c.BeginTransaction())
+            {
+                using (SqlCommand cmdInsert = Product.sqlInsert(c))
+                {
+                    cmdInsert.Parameters["@ProductName"].Value = prod.ProductName;
+                    cmdInsert.Parameters["@UnitPrice"].Value = prod.UnitPrice;
+                    cmdInsert.Parameters["@UnitsInStock"].Value = prod.UnitsInStock;
+                    cmdInsert.Transaction = trx;
+                    prod.ProductID = (int)cmdInsert.ExecuteScalar();
+                }
+
+                using (SqlCommand cmdGet = Product.sqlGetById(c))
+                {
+
+                    cmdGet.Parameters["@ProductId"].Value = prod.ProductID;
+                    cmdGet.Transaction = trx;
+                    Product newProd = null;
+                    using (SqlDataReader dr = cmdGet.ExecuteReader())
+                    {
+                        dr.Read();
+                        newProd = new Product(dr);
+                        Assert.AreEqual(prod.ProductID, newProd.ProductID);
+                        Assert.AreEqual(prod.UnitPrice, newProd.UnitPrice);
+                        Assert.AreEqual(prod.UnitsInStock, newProd.UnitsInStock);
+                        Assert.AreEqual(prod.ProductName, newProd.ProductName);
+                    }
+                }
+                trx.Rollback();
+            }
+        }
+
+        [TestMethod]
+        public void test_product_price_update()
+        {
+            using (SqlTransaction trx = c.BeginTransaction())
+            {
+                using (SqlCommand cmd = Product.sqlGetById(c))
+                {
+                    
+                    cmd.Parameters["@ProductId"].Value = 7;
+                    cmd.Transaction = trx;
+                    Product p = null;
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dr.Read();
+                        p = new Product(dr);
+                        Assert.AreEqual(30.0m, p.UnitPrice);
+                    }
+
+                    p.UnitPrice = 78;
+                    using (SqlCommand cmdUpdate = Product.sqlUpdate(c))
+                    {
+                        cmdUpdate.Transaction = trx;
+                        cmdUpdate.Parameters["@ProductName"].Value = p.ProductName;
+                        cmdUpdate.Parameters["@UnitPrice"].Value = p.UnitPrice;
+                        cmdUpdate.Parameters["@UnitsInStock"].Value = p.UnitsInStock;
+                        cmdUpdate.Parameters["@ProductID"].Value = p.ProductID;
+
+                        cmdUpdate.ExecuteNonQuery();
+                    }
+                    cmd.Parameters["@ProductId"].Value = p.ProductID; // O mesmo cmd com novo Bind
+                    cmd.Transaction = trx;
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dr.Read();
+                        p = new Product(dr);
+                        Assert.AreEqual(78.0m, p.UnitPrice);
+                    }
+
+                }
+                trx.Rollback();
+            }
+        }
+
+    }
+}
